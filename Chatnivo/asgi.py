7da06d108d3bea268647c5d8488f70afc8c250d2
kerem-aprv <altunkerem200@gmail.com>
2025-05-29 18:55:37 +0300

    Fixing error from routing.py

--- Chatnivo/asgi.py
+++ Chatnivo/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Chatnivo project.
+ASGI config for Whisperer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -7,15 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-
 import os
 import django
 from channels.routing import get_default_application
-from . import routing 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 django.setup()
-#asgi ile communication halinde eszamanli gateway
+
 application = get_default_application()
-application = routing.application
-
-
