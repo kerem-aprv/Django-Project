0eb29771065ad1ca52a433479c8cfdcb1e0f55cf
kerem-aprv <altunkerem200@gmail.com>
2025-05-29 19:23:56 +0300

    Configuration node.js

--- Chatnivo/asgi.py
+++ Chatnivo/asgi.py
@@ -14,4 +14,4 @@ from channels.routing import get_default_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 django.setup()
 
-application = get_default_application()
+application = get_default_application() 
